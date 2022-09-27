b267294d7f8af0a8820ded7940f2e3529dd795fb
unknown <ayshaseyd@gmail.com>
2022-09-27 15:03:05 +0100

    all files

--- script.js
+++ script.js
@@ -25,3 +25,46 @@ let moneyPaidSoFar = 2000;
 console.log(`The total bill is £${totalMoney} and the money paid so far is £${moneyPaidSoFar}`);
 
 console.log("This is now %c the end of exercises.","color: orange;font-family: fantasy; font-style: Bold;background-color: black; padding: 10px")
+
+let f = 100;
+while (f <= 200) { 
+    f++;
+    console.log(`f = ${f}`);}
+
+let g = 100;
+    while (g <= 200) {
+        if (g % 2 == 0) {console.log("-")} 
+        else {console.log("*")} 
+        g++;
+        }
+
+let strictA = true;
+let strictB = 1;
+console.log(strictA == strictB);
+console.log(strictA === strictB);
+
+let strictC = true;
+let strictD = 1;
+console.log(strictC != strictD);
+console.log(strictC !== strictD);
+
+let age = 10;
+{
+    if (age >= 18 && age <= 65) {console.log("Age is in range");} 
+    else if (age < 18) {console.log("underage");} 
+    else {console.log("catch all else");}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
