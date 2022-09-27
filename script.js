bd4dd62a5ad915ce864a6aaa88ed664ea15e053d
unknown <ayshaseyd@gmail.com>
2022-09-27 12:42:10 +0100

    all files

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict'
+console.log("test");
+console.log("Aysha Bint Seyd");
+console.info("JJ");
+console.warn("UK");
+console.error("Aquarius");
+
+let myCar = "Audi";
+let myModel = "A5";
+console.log("My fav car is:" +myCar+ "and my model is:" + myModel);
+
+let a;
+let b = 12345;
+let c = 12344;
+let d = true;
+let e = {a:"javascript"};
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2000;
+console.log(`The total bill is £${totalMoney} and the money paid so far is £${moneyPaidSoFar}`);
+
+console.log("This is now %c the end of exercises.","color: orange;font-family: fantasy; font-style: Bold;background-color: black; padding: 10px")
