6a1c829018ebc908591cc0aaf5ec9a5071022e3d
unknown <ayshaseyd@gmail.com>
2022-10-04 16:45:00 +0100

    Still not complete

--- HTML.Java Advanced/Fetch request/Fetch/Fetch/FetchAxios.js
+++ HTML.Java Advanced/Fetch request/Fetch/Fetch/FetchAxios.js
@@ -20,18 +20,3 @@ function createUser() {
     }).then(res => console.log(res))
         .catch(err => console.error(err));
 }
-
-// axios.get(URL)
-// .then(response => console.log(response.data))
-// .catch(err => console.error(err));
-
-/*axios.get('https://reqres.in/api/users')
-.then(function(response) {
-if (response.status!== 200) {
-    console.log (`Looks like there was a problem.Status Code: ${response.status}`);
-} 
-console.log(response.data)
-})
-.catch(err => console.error (`Fetch error : -S ${err}`))*/
-
-
