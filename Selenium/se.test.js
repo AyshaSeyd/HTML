5478a50ebe34b5440e2e300854efcddc0c2a2f8a
unknown <ayshaseyd@gmail.com>
2022-10-21 11:59:51 +0100

    Almost done

--- Selenium/se.test.js
+++ Selenium/se.test.js
@@ -16,22 +16,35 @@ describe("selenium tests", function () {
   // });
 
 
-  it("should find turtles", async () => {
+  it("24", async () => {
     const driver = await new Builder().forBrowser("chrome").build();
     try {
-      await driver.get("https://www.bbc.co.uk/search");
-      const search = await driver.findElement(By.id("search-input"));
-      await search.sendKeys("turtles");
+      await driver.get("http://localhost:3000/testing/ex1");
+      const search = await driver.findElement(By.css("#root > div > div.container > div:nth-child(2) > div:nth-child(2) > input"));
+      await search.sendKeys("15", Key.ENTER);
       // await search.sendKeys(Key.ENTER);
-      await driver.findElement(By.css("#main-content > form > div > div > div > button")).click();
-      const result = await driver.findElement(By.css("#main-content > div.ssrcss-1v7bxtk-StyledContainer.enjd40x0 > div > div > ul > li:nth-child(1) > div > div > div.ssrcss-tq7xfh-PromoContent.e1f5wbog7 > div.ssrcss-1f3bvyz-Stack.e1y4nx260 > a > span > p > span"));
-      expect(await result.getText()).to.equal("The Man Who Made Mock Turtle Soup");
+      const result = await driver.findElement(By.css("#root > div > div.container > div:nth-child(2) > div.mt-4.input-group > input"));
+      expect(await result.getAttribute("value")).to.equal("24");
     } finally {
       await driver.quit();
     }
   })
 
-  it.skip("should find a surprise", async () => {
+  it("24", async () => {
+    const driver = await new Builder().forBrowser("chrome").build();
+    try {
+      await driver.get("http://localhost:3000/testing/ex1");
+      const search = await driver.findElement(By.css("#root > div > div.container > div:nth-child(2) > div:nth-child(2) > input"));
+      await search.sendKeys("15", Key.ENTER);
+      // await search.sendKeys(Key.ENTER);
+      const result = await driver.findElement(By.css("#root > div > div.container > div:nth-child(2) > div.mt-4.input-group > input"));
+      expect(await result.getAttribute("value")).to.equal("24");
+    } finally {
+      await driver.quit();
+    }
+  })
+
+  /*it.skip("should find a surprise", async () => {
     const driver = await new Builder().forBrowser("chrome").build();
     try {
       driver.get("https://christophperrins.github.io/TestingSite/");
@@ -41,7 +54,7 @@ describe("selenium tests", function () {
       driver.quit();
     }
     return;
-  });
+  });*/
   //terminating the browser after each test
   // afterEach(async () => {
   //   await driver.quit();
